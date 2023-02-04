c4d676228d88a4348d85c7687d03418a35b5ceda
Pradeep Khanal <pradeepkhanal642@gmail.com>
2023-02-04 21:11:07 +1100

    styling finished

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
 // Getting Systems Time instead of launch time
 
-setInterval(() => {
-  const date = new Date();
-  let day = date.getDate();
-  let seconds = date.getSeconds();
-  let min = date.getMinutes();
-  let hour = date.getHours();
+// setInterval(() => {
+//   const date = new Date();
+//   let day = date.getDate();
+//   let seconds = date.getSeconds();
+//   let min = date.getMinutes();
+//   let hour = date.getHours();
 
-  day = day < 10 ? "0" + day : day;
-  min = min < 10 ? "0" + min : min;
-  hour = hour < 10 ? "0" + hour : hour;
-  seconds = seconds < 10 ? "0" + seconds : seconds;
+//   day = day < 10 ? "0" + day : day;
+//   min = min < 10 ? "0" + min : min;
+//   hour = hour < 10 ? "0" + hour : hour;
+//   seconds = seconds < 10 ? "0" + seconds : seconds;
 
-  // -----Days section--------------
+//   // -----Days section--------------
 
-  document.querySelector(".days-top").innerText = day;
-  document.querySelector(".days-bottom").innerText = day;
-  document.querySelector(".top-flip").innerText = day;
-  document.querySelector(".bottom-flip").innerText = day;
+//   document.querySelector(".days-top").innerText = day;
+//   document.querySelector(".days-bottom").innerText = day;
+//   document.querySelector(".top-flip").innerText = day;
+//   document.querySelector(".bottom-flip").innerText = day;
 
-  // -----Hours section--------------
+//   // -----Hours section--------------
 
-  document.querySelector(".hours-top").innerText = hour;
-  document.querySelector(".hours-bottom").innerText = hour;
-  // document.querySelector(".top-flip").innerText = hour;
-  // document.querySelector(".bottom-flip-hours").innerText = hour;
+// document.querySelector(".hours-top").innerText = hour;
+// document.querySelector(".hours-bottom").innerText = hour;
+// document.querySelector(".top-flip").innerText = hour;
+// document.querySelector(".bottom-flip-hours").innerText = hour;
 
-  // -----Mins section--------------
+// -----Mins section--------------
 
-  document.querySelector(".minutes-top").innerText = min;
-  document.querySelector(".minutes-bottom").innerText = min;
-  // document.querySelector(".top-flip-minutes").innerText = min;
-  // document.querySelector(".bottom-flip-minutes").innerText = min;
+//   document.querySelector(".minutes-top").innerText = min;
+//   document.querySelector(".minutes-bottom").innerText = min;
+//   // document.querySelector(".top-flip-minutes").innerText = min;
+//   // document.querySelector(".bottom-flip-minutes").innerText = min;
 
-  // -----Secondss section--------------
+//   // -----Secondss section--------------
 
-  document.querySelector(".seconds-top").innerText = seconds;
-  document.querySelector(".seconds-bottom").innerText = seconds;
-  // document.querySelector(".top-flip").innerText = seconds;
-  // document.querySelector(".bottom-flip").innerText = seconds;
-}, 1000);
+//   document.querySelector(".seconds-top").innerText = seconds;
+//   document.querySelector(".seconds-bottom").innerText = seconds;
+//   // document.querySelector(".top-flip").innerText = seconds;
+//   // document.querySelector(".bottom-flip").innerText = seconds;
+// }, 1000);
 
 // const topHalfs = document.querySelectorAll(".top-flip");
 // const bottomHalfs = document.querySelectorAll(".bottom-flip");
