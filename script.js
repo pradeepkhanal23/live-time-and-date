7d0dd106823159ef2dff7eae67778fcf92c78fb5
Pradeep Khanal <pradeepkhanal642@gmail.com>
2023-02-06 13:24:38 +1100

    final css and animation done excluding javascript

--- script.js
+++ script.js
@@ -12,43 +12,32 @@
 //   hour = hour < 10 ? "0" + hour : hour;
 //   seconds = seconds < 10 ? "0" + seconds : seconds;
 
-//   // -----Days section--------------
+//   const topSec = document.querySelector(".top-sec");
+//   const bottomSec = document.querySelector(".bottom-sec");
+//   topSec.innerHTML = seconds;
+//   bottomSec.textContent = seconds;
 
-//   document.querySelector(".days-top").innerText = day;
-//   document.querySelector(".days-bottom").innerText = day;
-//   document.querySelector(".top-flip").innerText = day;
-//   document.querySelector(".bottom-flip").innerText = day;
+//   const topHours = document.querySelector(".top-hr");
+//   const bottomHours = document.querySelector(".bottom-hr");
+//   topHours.innerHTML = hour;
+//   bottomHours.textContent = hour;
 
-//   // -----Hours section--------------
+//   const topMins = document.querySelector(".top-mins");
+//   const bottomMins = document.querySelector(".bottom-mins");
+//   topMins.innerHTML = min;
+//   bottomMins.textContent = min;
 
-// document.querySelector(".hours-top").innerText = hour;
-// document.querySelector(".hours-bottom").innerText = hour;
-// document.querySelector(".top-flip").innerText = hour;
-// document.querySelector(".bottom-flip-hours").innerText = hour;
+//   const topDays = document.querySelector(".top-days");
+//   const bottomDays = document.querySelector(".bottom-days");
+//   topDays.innerHTML = day;
+//   bottomDays.textContent = day;
 
-// -----Mins section--------------
+//   const secondCard = Array.from(
+//     document.querySelector(".flip-card-sec").children
+//   );
 
-//   document.querySelector(".minutes-top").innerText = min;
-//   document.querySelector(".minutes-bottom").innerText = min;
-//   // document.querySelector(".top-flip-minutes").innerText = min;
-//   // document.querySelector(".bottom-flip-minutes").innerText = min;
-
-//   // -----Secondss section--------------
-
-//   document.querySelector(".seconds-top").innerText = seconds;
-//   document.querySelector(".seconds-bottom").innerText = seconds;
-//   // document.querySelector(".top-flip").innerText = seconds;
-//   // document.querySelector(".bottom-flip").innerText = seconds;
-// }, 1000);
-
-// const topHalfs = document.querySelectorAll(".top-flip");
-// const bottomHalfs = document.querySelectorAll(".bottom-flip");
-
-// const topHalfArray = Array.from(topHalfs);
-// const bottomHalfArray = Array.from(bottomHalfs);
-
-// topHalfArray.forEach((top) => {
-//   top.addEventListener("onClick", () => {
-//     console.log(top);
+//   secondCard.forEach((card) => {
+//     card.dataset.count = seconds;
+//     console.log(card);
 //   });
-// });
+// }, 1000);
