32b26b2c2e4154697f9c5b37a6beb3aaf67eb53a
Pradeep Khanal <pradeepkhanal642@gmail.com>
2023-02-06 16:00:10 +1100

    flip animation left with scripting

--- script.js
+++ script.js
@@ -12,32 +12,48 @@
 //   hour = hour < 10 ? "0" + hour : hour;
 //   seconds = seconds < 10 ? "0" + seconds : seconds;
 
-//   const topSec = document.querySelector(".top-sec");
-//   const bottomSec = document.querySelector(".bottom-sec");
-//   topSec.innerHTML = seconds;
-//   bottomSec.textContent = seconds;
+//   // const topSec = document.querySelector(".top-sec");
+//   // const bottomSec = document.querySelector(".bottom-sec");
+//   // topSec.innerHTML = seconds;
+//   // bottomSec.textContent = seconds;
 
-//   const topHours = document.querySelector(".top-hr");
-//   const bottomHours = document.querySelector(".bottom-hr");
-//   topHours.innerHTML = hour;
-//   bottomHours.textContent = hour;
+//   // const topHours = document.querySelector(".top-hr");
+//   // const bottomHours = document.querySelector(".bottom-hr");
+//   // topHours.innerHTML = hour;
+//   // bottomHours.textContent = hour;
 
-//   const topMins = document.querySelector(".top-mins");
-//   const bottomMins = document.querySelector(".bottom-mins");
-//   topMins.innerHTML = min;
-//   bottomMins.textContent = min;
+//   // const topMins = document.querySelector(".top-mins");
+//   // const bottomMins = document.querySelector(".bottom-mins");
+//   // topMins.innerHTML = min;
+//   // bottomMins.textContent = min;
 
-//   const topDays = document.querySelector(".top-days");
-//   const bottomDays = document.querySelector(".bottom-days");
-//   topDays.innerHTML = day;
-//   bottomDays.textContent = day;
+//   // const topDays = document.querySelector(".top-days");
+//   // const bottomDays = document.querySelector(".bottom-days");
+//   // topDays.innerHTML = day;
+//   // bottomDays.textContent = day;
 
 //   const secondCard = Array.from(
 //     document.querySelector(".flip-card-sec").children
 //   );
 
 //   secondCard.forEach((card) => {
-//     card.dataset.count = seconds;
-//     console.log(card);
+//     card.dataset.digit = seconds;
+//   });
+// }, 1000);
+
+// setInterval(() => {
+//   const date = new Date();
+//   const seconds = date.getSeconds();
+//   const secondCardTop = document.querySelector(".top-sec");
+//   const secondCardBottom = document.querySelector(".bottom-sec");
+//   secondCardTop.innerHTML = seconds;
+//   secondCardBottom.innerHTML = seconds;
+//   secondCardTop.dataset.digit = seconds;
+//   secondCardBottom.dataset.digit = seconds;
+
+//   const flipCard = document.querySelector(".flip-card");
+//   flipCard.addEventListener("DOMCOntentLoaded", (e) => {
+//     flipCard.classList.add("flip");
+//     console.log("DOM fully loaded and parsed");
 //   });
 // }, 1000);
